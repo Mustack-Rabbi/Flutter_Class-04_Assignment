47fb4000096a8e2e9949ae563b1a00af4ded85d2
mustack-rabbi <mustackrabbi@gmail.com>
2022-09-04 13:59:55 +0600

    add packeg.. etc

--- lib/screen/bottom_bar.dart
+++ lib/screen/bottom_bar.dart
@@ -1,7 +1,11 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
 
 class BottomBar extends StatefulWidget {
-  const BottomBar({Key? key}) : super(key: key);
+  const BottomBar({Key? key, required this.onTap, required this.currentIndext})
+      : super(key: key);
+  final void Function(int) onTap;
+  final int currentIndext;
 
   @override
   State<BottomBar> createState() => _BottomBarState();
@@ -11,17 +15,27 @@ class _BottomBarState extends State<BottomBar> {
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: widget.currentIndext,
+        onTap: widget.onTap,
         elevation: 10,
         showSelectedLabels: false,
         showUnselectedLabels: false,
         selectedItemColor: Colors.green,
         unselectedItemColor: Colors.black,
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
           BottomNavigationBarItem(
+              icon: Icon(
+                FluentSystemIcons.ic_fluent_home_regular,
+              ),
+              activeIcon: Icon(FluentSystemIcons.ic_fluent_home_add_filled),
+              label: "Home"),
+          const BottomNavigationBarItem(
+              icon: Icon(Icons.search), label: "Search"),
+          const BottomNavigationBarItem(
               icon: Icon(Icons.airplane_ticket), label: "Ticket"),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+          const BottomNavigationBarItem(
+              icon: Icon(Icons.person), label: "Profile"),
         ]);
   }
 }
--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,15 +1,40 @@
 import 'package:class_04_assignment/screen/bottom_bar.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  static final List<Widget> _widgetOptions = <Widget>[
+    const Text("Home"),
+    const Text("Search"),
+    const Text("Ticket"),
+    const Text("Profile"),
+  ];
+
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+
+    print('$_selectedIndex');
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Class 04 Assignment")),
-      body: Center(child: Text("data")),
-      bottomNavigationBar: BottomBar(),
+      appBar: AppBar(title: const Text("Class 04 Assignment")),
+      body: Center(child: HomePage._widgetOptions[_selectedIndex]),
+      bottomNavigationBar: BottomBar(
+        onTap: _onItemTapped,
+        currentIndext: _selectedIndex,
+      ),
     );
   }
 }
